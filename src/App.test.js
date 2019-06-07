f7811e4ed31234f83ab701514de96847cfc8d729
Yonatan Rojas S <yonatan@bedu.org>
2019-06-07 18:16:18 -0500

    added test to render correctly component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-// Sintaxis de cómo se escribe una prueba en Jest
+import React from "react";
+import renderer from "react-test-renderer";
+import App from './App'
 
+// Sintaxis de cómo se escribe una prueba en Jest
 describe("Sintaxis de cómo se usa Jest", () => {
   it("Suma 2 números", () => {
     expect(1 + 2).toEqual(3);
     expect(2 + 2).toEqual(4);
   });
+});
+
+// El componente se muestra de manera correcta
+test("El componente se muestra", () => {
+  const component = renderer.create(<App />);
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
