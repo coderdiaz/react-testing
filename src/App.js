475cd7b34461d42a63fcb6391bd78f4b5c62c27c
Yonatan Rojas S <yonatan@bedu.org>
2019-06-07 19:30:02 -0500

    created Counter component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card/Card';
+import Counter from './Counter/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Card />
+        <Counter />
       </header>
     </div>
   );
